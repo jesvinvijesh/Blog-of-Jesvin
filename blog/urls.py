35e55cdbc95b7a87a6d66d03e9af3fc1f597c73d
Jesvin Vijesh S <msjjesvin@gmail.com>
2020-10-31 11:26:07 +0530

    make it better

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from blog.views import *
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('create/', create_blog_view, name="create"),
+    path('<slug>/', detail_blog_view, name="detail"),
+    path('<slug>/edit/', edit_blog_view, name="edit"),
+ ]
---
+++ personal/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+# class question(models.Model):
+# 	priority = [
+# 	('H','High'),
+# 	('M','Medium'), 
+# 	('L','Low')
+# 	]
+# 	title					=models.CharField(max_length=60)
+# 	question			    =models.TextField()
+# 	priority				=models.CharField(max_length=1, choices=priority)
+
+# 	def __str__(self):
+# 		return self.title
