9b1803df3a49d088c0468b203dd90930eb975fc7
Jesvin Vijesh S <msjjesvin@gmail.com>
2020-11-03 12:30:52 +0530

    added delete post

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('create/', create_blog_view, name="create"),
     path('<slug>/', detail_blog_view, name="detail"),
+    path('<slug>/delete', delete, name="delete"),
     path('<slug>/edit/', edit_blog_view, name="edit"),
  ]
